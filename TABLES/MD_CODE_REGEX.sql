2672bd2b9dffa0e8babf942183ba5d3d20c0a0ff
razanHamzeh <razan_f_hamze@yahoo.com>
2017-04-04 10:16:07 +0300

    Add files via upload

---
+++ TABLES/MD_CODE_REGEX.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  Constraints for Table MD_CODE_REGEX
+--------------------------------------------------------
+
+  ALTER TABLE "HISRHAMZEH"."MD_CODE_REGEX" MODIFY ("DESCRIPTION" NOT NULL ENABLE);
+  ALTER TABLE "HISRHAMZEH"."MD_CODE_REGEX" MODIFY ("REGEX" NOT NULL ENABLE);
+  ALTER TABLE "HISRHAMZEH"."MD_CODE_REGEX" MODIFY ("ID" NOT NULL ENABLE);
---
+++ TABLES/MD_REPOVERSIONS.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  Constraints for Table MD_REPOVERSIONS
+--------------------------------------------------------
+
+  ALTER TABLE "HISRHAMZEH"."MD_REPOVERSIONS" MODIFY ("REVISION" NOT NULL ENABLE);
