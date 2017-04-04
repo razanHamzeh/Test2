16ef2f2ca2e40bf914c7a6374ca07c958c272aae
razanHamzeh <razan_f_hamze@yahoo.com>
2017-04-04 10:11:41 +0300

    Add files via upload

---
+++ TABLES/AUDIT_LOG.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  DDL for Table AUDIT_LOG
+--------------------------------------------------------
+
+  CREATE TABLE "HISRHAMZEH"."AUDIT_LOG" 
+   (	"OPERATION" VARCHAR2(25 BYTE), 
+	"OWNER" VARCHAR2(25 BYTE), 
+	"NAME" VARCHAR2(25 BYTE), 
+	"EXTRA" VARCHAR2(4000 BYTE)
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
