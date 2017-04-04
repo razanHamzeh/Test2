74e08e95ffcf68457787c09bb4eb9706f512f9aa
razanHamzeh <razan_f_hamze@yahoo.com>
2017-04-04 10:32:37 +0300

    Delete MIGRATION_RESERVED_WORDS.sql

--- TABLES/MIGRATION_RESERVED_WORDS.sql
+++
@@ -1,5 +0,0 @@
---------------------------------------------------------
---  Constraints for Table MIGRATION_RESERVED_WORDS
---------------------------------------------------------
-
-  ALTER TABLE "HISRHAMZEH"."MIGRATION_RESERVED_WORDS" MODIFY ("KEYWORD" NOT NULL ENABLE);
